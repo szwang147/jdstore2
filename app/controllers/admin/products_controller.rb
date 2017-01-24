d099c9d6ca755bb4c44c1540f45b2ebbc0215592
simon <wangzd.fudan@gmail.com>
2017-01-24 21:00:02 +0800

    crud for products and 4pages for views

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,50 @@
+class Admin::ProductsController < ApplicationController
+  before_action :authenticate_use!
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path
+      flash[:notice] = "create"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_products_path
+      flash[:notice] = "update"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path
+    flash[:notice] = "delete"
+  end
+  
+private
+  def group_params
+    params.require(:group).permit(:title, :description, :quantity, :price)
+  end
+end
