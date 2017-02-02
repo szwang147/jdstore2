4997505d53559edd0d7bbf5f5f4c241aefdc6fcd
simon <wangzd.fudan@gmail.com>
2017-02-02 20:49:39 +0800

    implement account history order

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,10 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :account do
+    resources :orders
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
   resources :welcome
