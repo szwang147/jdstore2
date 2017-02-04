555523102abcba057da7a24096c798f2c15170f6
simon <wangzd.fudan@gmail.com>
2017-02-04 11:20:41 +0800

    implement statement

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -35,7 +35,7 @@ class OrdersController < ApplicationController
   def pay_with_wechat
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("wechat")
-    @order.pay!
+    @order.make_payment!
 
     redirect_to order_path(@order.token), notice:"by wechat"
   end
@@ -43,8 +43,7 @@ class OrdersController < ApplicationController
   def pay_with_alipay
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("alipay")
-    @order.pay!
-
+    @order.make_payment!
     redirect_to order_path(@order.token), notice:"by alipay"
   end
 
